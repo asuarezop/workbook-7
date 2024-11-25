f5903d894220653939efe87b2af96fa88719ee97
Anthony Suarez <asuarezop@gmail.com>
2024-11-25 10:10:09 -0500

    Added in new queries from workbook-7

--- workbook-7/joins/join_queries.sql
+++ workbook-7/joins/join_queries.sql
@@ -5,3 +5,17 @@ SELECT ProductID, ProductName, UnitPrice, CategoryName FROM Products
 JOIN Categories ON Products.CategoryID = Categories.CategoryID
 ORDER BY CategoryName, ProductName;
 
+-- Querying for products that have a unit price greater than $75, joined with Suppliers to view CompanyName
+SELECT ProductID, ProductName, UnitPrice, CompanyName FROM Products
+JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+WHERE UnitPrice > 75 
+ORDER BY ProductName;
+
+-- Querying for products with multiple properties from 3 different tables (Products, Suppliers, and Categories)
+SELECT ProductID, ProductName, UnitPrice, CategoryName, CompanyName FROM Products
+JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+JOIN Categories ON Products.CategoryID = Categories.CategoryID
+ORDER BY ProductName;
+
+
+
