9f50e7e4a7dc933aefad2d91522fd9067444dffc
Anthony Suarez <asuarezop@gmail.com>
2024-11-25 17:10:11 -0500

    Added finished join queries for workbook-7

--- workbook-7/joins/join_queries.sql
+++ workbook-7/joins/join_queries.sql
@@ -17,5 +17,48 @@ JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
 JOIN Categories ON Products.CategoryID = Categories.CategoryID
 ORDER BY ProductName;
 
+-- Querying for the most expensive product from all categories
+SELECT ProductName, CategoryName, UnitPrice
+FROM Products
+JOIN Categories ON Products.CategoryID = Categories.CategoryID
+WHERE UnitPrice = (SELECT MAX(UnitPrice) FROM Products
+GROUP BY CategoryName
+);
 
+-- Alternate SOLUTION
+-- Select CategoryID,ProductName,UnitPrice
+-- from Products AS p1
+-- where UnitPrice = (select  MAX(UnitPrice) from Products AS p2
+-- 					where p1.CategoryID = p2.CategoryID
+-- 					)
+-- order by UnitPrice desc;
 
+-- Querying for all the most expensive products from each category
+SELECT Products.ProductName, CategoryName, MAX(UnitPrice) AS MaximumPrice
+FROM Categories
+JOIN Products ON Categories.CategoryID = Products.CategoryID
+WHERE Products.UnitPrice = (SELECT MAX(UnitPrice) FROM Products
+WHERE Products.CategoryID = (SELECT Categories.CategoryID ))
+GROUP BY CategoryName, Products.ProductName;
+
+-- Querying for all the orders that have shipped from Germany
+SELECT OrderID, ShipName, ShipAddress, ShipCountry, CompanyName FROM Shippers
+LEFT JOIN Orders ON Orders.ShipVia = Shippers.ShipperID
+WHERE ShipCountry = 'Germany';
+
+-- Querying for all the orders that contain product 'Sasquatch Ale'
+SELECT Orders.OrderID, OrderDate, ShipName, ShipAddress, `Order Details`.ProductID, Products.ProductName 
+FROM Orders
+JOIN `Order Details` ON `Order Details`.OrderID = Orders.OrderID
+JOIN Products ON Products.ProductID = `Order Details`.ProductID 
+WHERE Products.ProductName = 'Sasquatch Ale';
+    
+-- Alternative implementation to find all orders with product 'Sasquatch Ale' using Nested queries    
+SELECT Orders.OrderID, OrderDate, ShipName
+FROM Orders
+WHERE Orders.OrderID IN 
+  (SELECT OrderID FROM `Order Details`
+  WHERE ProductID = 
+    (SELECT ProductID 
+     FROM Products
+     WHERE ProductName = 'Sasquatch Ale'));
